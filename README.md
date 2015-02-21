b2e3fbf48569e4dee88ac2423cca77b1091aa84c
inaniwa3 <inaniwa3@gmail.com>
2015-02-21 14:10:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Quiet Twitter
+
+A simple Chrome extension to hide some of the information on Twitter.
+
+## Screenshot
+
+Before
+
+![before.png](before.png "before.png")
+
+After
+
+![after.png](after.png "after.png")
+
+## References
+
+[ツイッターのフォロー・フォロワー・ツイート数を非表示にする](http://www.jfkooya.com/jiyoo/2012/03/hide-the-number-of-followings-and-followers-and-tweets-of-you-on-twitter.html "ツイッターのフォロー・フォロワー・ツイート数を非表示にする")  
+[Hide Twitter Guff](https://chrome.google.com/webstore/detail/hide-twitter-guff/ebjehgoicideedhhnfjhfaidlpdhofod "Hide Twitter Guff")
---
+++ quiet-twitter/quiet-twitter.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Quiet Twitter
+// @namespace   https://twitter.com/inaniwa3
+// @description Hide some of the information on Twitter.
+// @include     http://twitter.com/*
+// @include     https://twitter.com/*
+// @version     1.0.0
+// ==/UserScript==
+
+// References
+// ツイッターのフォロー・フォロワー・ツイート数を非表示にする
+// http://www.jfkooya.com/jiyoo/2012/03/hide-the-number-of-followings-and-followers-and-tweets-of-you-on-twitter.html
+// Hide Twitter Guff
+// https://chrome.google.com/webstore/detail/hide-twitter-guff/ebjehgoicideedhhnfjhfaidlpdhofod
+
+var newStyle = document.createElement("style");
+document.getElementsByTagName("head")[0].appendChild(newStyle); 
+newStyle.appendChild(document.createTextNode(
+  ".wtf-module{display:none !important;}"+                      // おすすめユーザー
+  ".trends{display:none !important;}"+                          // トレンド
+  ".DashboardProfileCard-statValue{display:none !important;}"+  // ツイート、フォロー、フォロワー
+  ".ProfileWTFAndTrends{display:none !important;}"+             // おすすめユーザー、トレンド
+  ".ProfileNav-value{display:none !important;}"+                // ツイート、フォロー、フォロワー、お気に入り、リスト
+  ".js-mini-profile-stat{display:none !important;}"             // ツイート、フォロー、フォロワー
+));
