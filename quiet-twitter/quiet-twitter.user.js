093b9d2443f6015024d892b924c7b5044266d14a
inaniwa3 <inaniwa3@gmail.com>
2018-11-23 20:44:33 +0900

    version 2.0.0

--- quiet-twitter/quiet-twitter.user.js
+++ quiet-twitter/quiet-twitter.user.js
@@ -4,25 +4,30 @@
 // @description Hide some of the information on Twitter.
 // @include     http://twitter.com/*
 // @include     https://twitter.com/*
-// @version     1.4.0
+// @version     2.0.0
 // ==/UserScript==
 
 // References
 // ツイッターのフォロー・フォロワー・ツイート数を非表示にする
-// http://www.jfkooya.com/jiyoo/2012/03/hide-the-number-of-followings-and-followers-and-tweets-of-you-on-twitter.html
+// https://www.jfkooya.com/jiyoo/2012/03/hide-the-number-of-followings-and-followers-and-tweets-of-you-on-twitter.html
 // Hide Twitter Guff
 // https://chrome.google.com/webstore/detail/hide-twitter-guff/ebjehgoicideedhhnfjhfaidlpdhofod
 
-var newStyle = document.createElement("style");
-document.getElementsByTagName("head")[0].appendChild(newStyle); 
-newStyle.appendChild(document.createTextNode(
-  ".wtf-module{display:none !important;}"+                      // おすすめユーザー
-  ".trends{display:none !important;}"+                          // トレンド
-  ".DashboardProfileCard-statValue{display:none !important;}"+  // ツイート、フォロー、フォロワー
-  ".ProfileWTFAndTrends{display:none !important;}"+             // おすすめユーザー、トレンド
-  ".ProfileNav-value{display:none !important;}"+                // ツイート、フォロー、フォロワー、お気に入り、リスト
-  ".ProfileCardStats-statValue{display:none !important;}"+      // ツイート、フォロー、フォロワー
-  ".WhoToFollow{display:none !important;}"+                     // おすすめユーザー
-  ".TweetImpressionsModule{display:none !important;}"+          // Your Tweet activity
-  ".LiveVideoHomePageModule{display:none !important;}"          // Live video
-));
+var text = ""
+    + ".wtf-module { display:none !important; }\n"
+    + ".Trends { display:none !important; }\n"
+    + ".TweetImpressionsModule { display:none !important; }\n"
+    + ".LiveVideoHomePageModuleContainer { display:none !important; }\n"
+    + ".ProfileNav-value { display:none !important; }\n"
+    + ".ProfileCardStats-statValue { display:none !important; }\n"
+    + ".ProfileTweet-actionCountForPresentation { display:none !important; }\n"
+    + ".request-retweeted-popup strong { display:none !important; }\n"
+    + ".request-favorited-popup strong { display:none !important; }\n"
+    + ".rn-lrvibr span { display:none !important; }\n"
+    + ".js-translate-tweet { display:none !important; }\n"
+    + ".MomentCapsuleLikesCount { display:none !important; }\n"
+    ;
+
+var elem = document.createElement("style");
+document.getElementsByTagName("head")[0].appendChild(elem); 
+elem.appendChild(document.createTextNode(text));
